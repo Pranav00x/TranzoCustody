7ec7b0865547cc607355bc3d280f058b28f740a8
Pranav <pranavjha0002@gmail.com>
2026-04-08 12:14:33 +0530

    fix: add missing clickable import in CreateImportScreen

--- app/src/main/java/com/tranzo/custody/ui/onboarding/CreateImportScreen.kt
+++ app/src/main/java/com/tranzo/custody/ui/onboarding/CreateImportScreen.kt
@@ -2,6 +2,7 @@ package com.tranzo.custody.ui.onboarding
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
